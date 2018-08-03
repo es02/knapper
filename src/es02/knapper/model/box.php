2e75b713d990e761a33b9e8b66f2b5b1f71cc3b8
ArachnidsGrip <minervavine@gmail.com>
2018-08-03 15:51:43 +1000

    Simplify models
    Moved bulk of model code into a primitive class and extended the two exisiting models from that.
    Cuts code reuse significantly

--- src/es02/knapper/model/box.php
+++ src/es02/knapper/model/box.php
@@ -2,23 +2,8 @@
 
 namespace es02\Knapper\model;
 
-class Box
+class Box extends Primitive
 {
-    /**
-     * Sets gross box measurements
-     * @var float
-     */
-    public $length = 0.0;
-    public $width = 0.0;
-    public $height = 0.0;
-    public $weight = 0.0;
-
-    /**
-     * Optional identifier
-     * @var string
-     */
-    public $name = null;
-
     /**
      * Do we have a limited number of this box size?
      * Assume no.
@@ -26,49 +11,4 @@ class Box
      */
     public $quantity = 0;
     public $remaining = 0;
-
-    /**
-     * Store calculated cubic measurement
-     * @var float
-     */
-    public $cubic = 0.0;
-
-    /**
-     * Sets measurement system and units (Metric/Imperial)(Small/Large)
-     * @var string
-     */
-    public $lengthType = 'cm'; // cm, m, in, ft
-    public $weightType = 'g';  // g, kg, oz, lb
-
-    /**
-     * Determines if we process in 3D or 2D
-     * important for fragile and dangerous goods.
-     * @var bool
-     */
-    public $thisWayUp =  false;
-
-    public function __construct(array $box)
-    {
-        $this->length = $box['length'];
-        $this->width = $box['width'];
-        $this->height = $box['height'];
-        $this->weight = $box['weight'];
-
-        // If not set leave on sane defaults
-        if (isset($box['lengthType']) and !empty($box['lengthType'])) {
-            $this->lengthType = $box['lengthType'];
-        }
-        if (isset($box['weightType']) and !empty($box['weightType'])) {
-            $this->weightType = $box['weightType'];
-        }
-        if (isset($box['quantity']) and !empty($box['quantity'])) {
-            $this->quantity = $box['quantity'];
-            $this->remaining = $box['quantity'];
-        }
-        if (isset($item['name']) and !empty($item['name'])) {
-            $this->name = $item['name'];
-        }
-
-        $this->cubic = $box['length'] * $box['width'] * $box['height'];
-    }
 }
--- src/es02/knapper/model/item.php
+++ src/es02/knapper/model/item.php
@@ -2,43 +2,10 @@
 
 namespace es02\Knapper\model;
 
-class Item
+use es02\Knapper\controller\Utilities;
+
+class Item extends Primitive
 {
-    /**
-     * Sets gross item measurements
-     * @var float
-     */
-    public $length = 0.0;
-    public $width = 0.0;
-    public $height = 0.0;
-    public $weight = 0.0;
-
-    /**
-     * Optional identifier
-     * @var string
-     */
-    public $name = null;
-
-    /**
-     * Store calculated cubic measurement
-     * @var float
-     */
-    public $cubic = 0.0;
-
-    /**
-     * Sets measurement system and units (Metric/Imperial)(Small/Large)
-     * @var string
-     */
-    public $lengthType = 'cm'; // cm, m, in, ft
-    public $weightType = 'g';  // g, kg, oz, lb
-
-    /**
-     * Determines if we process in 3D or 2D
-     * important for fragile and dangerous goods.
-     * @var bool
-     */
-    public $thisWayUp =  false;
-
     /**
      * Once sorted we store box number here
      * @var integer
@@ -61,28 +28,4 @@ class Item
     public $x = 0;
     public $y = 0;
     public $z = 0;
-
-    public function __construct(array $item)
-    {
-        $this->length = $item['length'];
-        $this->width = $item['width'];
-        $this->height = $item['height'];
-        $this->weight = $item['weight'];
-
-        // If not set leave on sane defaults
-        if (isset($item['lengthType']) and !empty($item['lengthType'])) {
-            $this->lengthType = $item['lengthType'];
-        }
-        if (isset($item['weightType']) and !empty($item['weightType'])) {
-            $this->weightType = $item['weightType'];
-        }
-        if (isset($item['thisWayUp']) and !empty($item['thisWayUp'])) {
-            $this->thisWayUp = $item['thisWayUp'];
-        }
-        if (isset($item['name']) and !empty($item['name'])) {
-            $this->name = $item['name'];
-        }
-
-        $this->cubic = $item['length'] * $item['width'] * $item['height'];
-    }
 }
